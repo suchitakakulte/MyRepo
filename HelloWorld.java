2394cf58c293ed9efd364082f93bf68c752ff9e3
Suchita Kakulte <suchita.kakulte@ad.infosys.com>
2017-11-13 11:59:10 +0530

    commiting HelloWorld.java file

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+ public static void main(String args[]){
+     System.out.println("Hi..My First Java Program!");
+	 for(int i=0;i<=10;i++){
+	 System.out.println("Hello world executing ..."+i+" times!");
+ }
+}
+}
